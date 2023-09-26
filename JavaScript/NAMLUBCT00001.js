e5130df42ea7570960dfc8e2e29ea7ffaafac014
Nguyen Quang He <quanghe2003@gmail.com>
2023-09-26 13:25:09 +0700

    QuangHe_26092034_addproduct

---
+++ JavaScript/NAMLUBCT00001.js
@@ -0,0 +1,30 @@
+function color_black() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg  = document.getElementById("mainImage");  
+    
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NAMLUBCT00001/_01_black.jpeg";
+    secondImg1.src = "../Picture/Product/NAMLUBCT00001/_01_black.jpeg";
+    secondImg2.src = "../Picture/Product/NAMLUBCT00001/_02_black.jpeg";
+    secondImg3.src = "../Picture/Product/NAMLUBCT00001/_03_black.jpeg";
+    secondImg4.src = "../Picture/Product/NAMLUBCT00001/_04_black.jpeg";
+}
+function color_white() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg  = document.getElementById("mainImage");  
+    
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NAMLUBCT00001/_01_white.jpeg";
+    secondImg1.src = "../Picture/Product/NAMLUBCT00001/_01_white.jpeg";
+    secondImg2.src = "../Picture/Product/NAMLUBCT00001/_02_white.jpeg";
+    secondImg3.src = "../Picture/Product/NAMLUBCT00001/_03_white.jpeg";
+    secondImg4.src = "../Picture/Product/NAMLUBCT00001/_04_white.jpeg";
+}
