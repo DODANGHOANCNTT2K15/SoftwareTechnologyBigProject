144e56d0ffd21736942307be8e1c2b794c7add9a
Minh-N-D <gminh2772@gmail.com>
2023-09-27 21:50:05 +0700

    NguyenDucMinh

---
+++ JavaScript/NALTNVCT00044.js
@@ -0,0 +1,45 @@
+function color_green() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTNVCT00044/00044_1.jpg";
+    secondImg1.src = "../Picture/Product/NALTNVCT00044/00044_2.jpg";
+    secondImg2.src = "../Picture/Product/NALTNVCT00044/00044_3.jpg";
+    secondImg3.src = "../Picture/Product/NALTNVCT00044/00044_4.jpg";
+    secondImg4.src = "../Picture/Product/NALTNVCT00044/00044_5.jpg";
+}
+function color_yellow() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTNVCT00044/00044_w1.jpg";
+    secondImg1.src = "../Picture/Product/NALTNVCT00044/00044_w2.jpg";
+    secondImg2.src = "../Picture/Product/NALTNVCT00044/00044_w3.jpg";
+    secondImg3.src = "../Picture/Product/NALTNVCT00044/00044_w4.jpg";
+    secondImg4.src = "../Picture/Product/NALTNVCT00044/00044_w5.jpg";
+}
+function color_white() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTNVCT00044/00044_b1.jpg";
+    secondImg1.src = "../Picture/Product/NALTNVCT00044/00044_b2.jpg";
+    secondImg2.src = "../Picture/Product/NALTNVCT00044/00044_b3.jpg";
+    secondImg3.src = "../Picture/Product/NALTNVCT00044/00044_b4.jpg";
+    secondImg4.src = "../Picture/Product/NALTNVCT00044/00044_b5.jpg";
+}
