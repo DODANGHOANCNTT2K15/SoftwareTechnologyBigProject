7f0af655044334bd1da7bdf81c52c4a60fe46d90
nguyenkhai030717 <97429406+nguyenkhai030717@users.noreply.github.com>
2023-09-27 12:43:52 +0700

    nguyenkhai030717

---
+++ JavaScript/NALTUBCV00003.js
@@ -0,0 +1,30 @@
+function color_ghi() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTUBCV00003/_01_ghi.jpg";
+    secondImg1.src = "../Picture/Product/NALTUBCV00003/_01_ghi.jpg";
+    secondImg2.src = "../Picture/Product/NALTUBCV00003/_02_ghi.jpg";
+    secondImg3.src = "../Picture/Product/NALTUBCV00003/_03_ghi.jpg";
+    secondImg4.src = "../Picture/Product/NALTUBCV00003/_04_ghi.jpg";
+}
+function color_ghi() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTUBCV00003/_01_ghi.jpg";
+    secondImg1.src = "../Picture/Product/NALTUBCV00003/_01_ghi.jpg";
+    secondImg2.src = "../Picture/Product/NALTUBCV00003/_02_ghi.jpg";
+    secondImg3.src = "../Picture/Product/NALTUBCV00003/_03_ghi.jpg";
+    secondImg4.src = "../Picture/Product/NALTUBCV00003/_04_ghi.jpg";
+}
