bcd1507c582cb781794d91c4089ccec6f9b19d2d
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-06 23:17:36 +0700

    DODANGHOAN_06102023_IMF_USER

--- JavaScript/imf_user.js
+++ JavaScript/imf_user.js
@@ -13,17 +13,65 @@ add_user.addEventListener('click',function(){
     _address.style.display = 'block';
 });
 
+// sửa thông tin người dùng
 var edit_information_user = document.querySelector('#edit_imformation_user')
 var turnOn_form_imf_user = document.querySelector('.change_information_user')
 edit_information_user.addEventListener('click',function(){
     turnOn_form_imf_user.style.display = 'flex';
 });
 
-var exit_change_imf_user = document.querySelector('.exit_change_imf');
+var exit_change_imf_user = document.querySelector('.change_information_user>.exit_change_imf');
 exit_change_imf_user.addEventListener('click',function(){
     turnOn_form_imf_user.style.display = 'none';
 });
 
-var exit = function(){
-    turnOn_form_imf_user.style.display = 'none';
-}
+
+//sửa email người dùng
+var edit_Email_user = document.querySelector('#edit_imformation_user_1')
+var turnOn_form_Email_user = document.querySelector('.change_Email_user')
+edit_Email_user.addEventListener('click',function(){
+    turnOn_form_Email_user.style.display = 'flex';
+});
+
+var exit_change_Email_user = document.querySelector('.change_Email_user>.exit_change_imf');
+exit_change_Email_user.addEventListener('click',function(){
+    turnOn_form_Email_user.style.display = 'none';
+});
+
+
+// sửa passwork người dùng
+var edit_Passwork_user = document.querySelector('#edit_imformation_user_2')
+var turnOn_form_Passwork_user = document.querySelector('.change_Passwork_imf')
+edit_Passwork_user.addEventListener('click',function(){
+    turnOn_form_Passwork_user.style.display = 'flex';
+});
+
+var exit_change_Passwork_user = document.querySelector('.change_Passwork_imf>.exit_change_imf');
+exit_change_Passwork_user.addEventListener('click',function(){
+    turnOn_form_Passwork_user.style.display = 'none';
+});
+
+
+// sửa số điện thoại người dùng
+var edit_Phone_user = document.querySelector('#edit_imformation_user_3')
+var turnOn_form_Phone_user = document.querySelector('.change_Phone_imf')
+edit_Phone_user.addEventListener('click',function(){
+    turnOn_form_Phone_user.style.display = 'flex';
+});
+
+var exit_change_Phone_user = document.querySelector('.change_Phone_imf>.exit_change_imf');
+exit_change_Phone_user.addEventListener('click',function(){
+    turnOn_form_Phone_user.style.display = 'none';
+});
+
+// Thêm địa chỉ
+var edit_Address_user = document.querySelector('.address_add')
+var turnOn_form_Address_user = document.querySelector('.add_address_user')
+edit_Address_user.addEventListener('click',function(){
+    turnOn_form_Address_user.style.display = 'flex';
+});
+
+var exit_change_Address_user = document.querySelector('.add_address_user>.exit_change_imf');
+exit_change_Address_user.addEventListener('click',function(){
+    turnOn_form_Address_user.style.display = 'none';
+});
