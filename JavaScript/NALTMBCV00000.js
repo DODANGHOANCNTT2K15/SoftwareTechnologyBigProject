243576c8d46d77912744905d129bc96f27a71590
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-24 01:05:38 +0700

    Product

---
+++ JavaScript/NALTMBCV00000.js
@@ -0,0 +1,46 @@
+function color_ghi() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTMBCV00000/01_Green.jpeg";
+    secondImg1.src = "../Picture/Product/NALTMBCV00000/01_Green.jpeg";
+    secondImg2.src = "../Picture/Product/NALTMBCV00000/02_Green.jpeg";
+    secondImg3.src = "../Picture/Product/NALTMBCV00000/03_Green.jpeg";
+    secondImg4.src = "../Picture/Product/NALTMBCV00000/04_Green.jpeg";
+}
+function color_ghi2() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTMBCV00000/01_White.jpeg";
+    secondImg1.src = "../Picture/Product/NALTMBCV00000/01_White.jpeg";
+    secondImg2.src = "../Picture/Product/NALTMBCV00000/02_White.jpeg";
+    secondImg3.src = "../Picture/Product/NALTMBCV00000/03_White.jpeg";
+    secondImg4.src = "../Picture/Product/NALTMBCV00000/04_White.jpeg";
+}
+
+function color_ghi3() {
+    // Lấy thẻ hình ảnh mục tiêu theo id
+    var secondImg1 = document.getElementById("secondImg1");
+    var secondImg2 = document.getElementById("secondImg2");
+    var secondImg3 = document.getElementById("secondImg3");
+    var secondImg4 = document.getElementById("secondImg4");
+    var mainImg = document.getElementById("mainImage");
+
+    // THay đổi đường link các ảnh
+    mainImg.src = "../Picture/Product/NALTMBCV00000/01_Yellow.jpeg";
+    secondImg1.src = "../Picture/Product/NALTMBCV00000/01_Yellow.jpeg";
+    secondImg2.src = "../Picture/Product/NALTMBCV00000/02_Yellow.jpeg";
+    secondImg3.src = "../Picture/Product/NALTMBCV00000/03_Yellow.jpeg";
+    secondImg4.src = "../Picture/Product/NALTMBCV00000/04_Yellow.jpeg";
+}
