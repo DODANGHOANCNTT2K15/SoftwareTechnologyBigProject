b7bd4f0101dc476b6a3cb12c588856b43e6c92f8
DO DANG HOAN <121910478+DODANGHOANCNTT2K15@users.noreply.github.com>
2023-10-26 01:03:15 +0700

    fix_1

---
+++ JavaScript/FindShop.js
@@ -0,0 +1,3 @@
+document.querySelector('.button_findLocation button').addEventListener('click', function() {
+    window.open('https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%90%E1%BA%A1i+H%E1%BB%8Dc+Phenikaa/@20.9626112,105.7461115,17z/data=!3m1!4b1!4m6!3m5!1s0x313452efff394ce3:0x391a39d4325be464!8m2!3d20.9626112!4d105.7486864!16s%2Fg%2F1220whsz?entry=ttu', '_blank');
+});
